af34e1ab2cc1001b682e49c7ee7e2b7306336fd1
amel <amelstutic@gmail.com>
2023-12-11 16:19:57 +0100

    Add db context

---
+++ GloBus-backend/Infrastructure/GloBusContext.cs
@@ -0,0 +1,15 @@
+﻿using GloBus_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloBus_backend.Infrastructure
+{
+    public class GloBusContext : DbContext
+    {
+        public GloBusContext(DbContextOptions<GloBusContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
