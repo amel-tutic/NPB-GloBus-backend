0d91ad4ab4966d04bd915100d27c9020c4ca0b5f
amel <amelstutic@gmail.com>
2023-12-11 16:21:06 +0100

    Add db context service

--- GloBus-backend/Program.cs
+++ GloBus-backend/Program.cs
@@ -1,6 +1,12 @@
+using GloBus_backend.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<GloBusContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("default")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
