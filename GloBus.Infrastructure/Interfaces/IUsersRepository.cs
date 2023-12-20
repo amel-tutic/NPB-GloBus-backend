e60b9bcfa6836b2409b5a691c017bcafb02a53cd
amel <amelstutic@gmail.com>
2023-12-20 15:39:53 +0100

    Add UserRepository and its interface, with AddUser task

---
+++ GloBus.Infrastructure/Interfaces/IUsersRepository.cs
@@ -0,0 +1,15 @@
+﻿using GloBus.Data.DTOs;
+using GloBus.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloBus.Infrastructure.Interfaces
+{
+    public interface IUsersRepository
+    {
+        Task<User> AddUser(UserDTO request);
+    }
+}
---
+++ GloBus.Infrastructure/Repositories/UsersRepository.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using GloBus.Data;
+using GloBus.Data.DTOs;
+using GloBus.Data.Models;
+using GloBus.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloBus.Infrastructure.Repositories
+{
+    public class UsersRepository : IUsersRepository
+    {
+        private readonly GloBusContext context;
+        private readonly IMapper mapper;
+
+        public UsersRepository(GloBusContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public async Task<User> AddUser(UserDTO userDTO)
+        {
+            User user = mapper.Map<User>(userDTO);
+
+            await context.Users.AddAsync(user);
+
+            await context.SaveChangesAsync();
+
+            user = await context.Users
+                .Where(u => u.Email == userDTO.Email)
+                .FirstOrDefaultAsync();
+
+            return user;
+        }
+    }
+}
