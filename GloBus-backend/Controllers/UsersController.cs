6fc55818fc632ff977e2dd39142b7a554db67ed4
amel <amelstutic@gmail.com>
2023-12-20 15:41:50 +0100

    Add User controller, with Add task to add a new user, route being "add"

---
+++ GloBus-backend/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using GloBus.Data.DTOs;
+using GloBus.Data.Models;
+using GloBus.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GloBus_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public UsersController(IUnitOfWork UnitOfWork)
+        {
+            unitOfWork = UnitOfWork;
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> Add(UserDTO request)
+        {
+            User user = await unitOfWork.UsersRepository.AddUser(request);
+
+            return Ok(user);
+        }
+    }
+}
