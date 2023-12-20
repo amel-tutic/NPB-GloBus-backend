34373d5d4c8f06b5afab689c395b84bc98872c96
amel <amelstutic@gmail.com>
2023-12-20 15:42:45 +0100

    Add UserProfile for DTO/Model mapping

---
+++ GloBus-backend/Profiles/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using GloBus.Data.DTOs;
+using GloBus.Data.Models;
+
+namespace GloBus_backend.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UserDTO, User>();
+            CreateMap<User, UserDTO>();
+        }
+    }
+}
