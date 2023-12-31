034d0a1af63a3a456042fa9cda6345d7cd613c32
amel <amelstutic@gmail.com>
2023-12-31 13:08:09 +0100

    Map User login and register DTOs to User

--- GloBus-backend/Profiles/UserProfile.cs
+++ GloBus-backend/Profiles/UserProfile.cs
@@ -10,6 +10,10 @@ namespace GloBus_backend.Profiles
         {
             CreateMap<UserDTO, User>();
             CreateMap<User, UserDTO>();
+            CreateMap<User, UserRegisterDTO>();
+            CreateMap<UserRegisterDTO, User>();
+            CreateMap<UserLoginDTO, User>();
+            CreateMap<User, UserLoginDTO>();
         }
     }
 }
