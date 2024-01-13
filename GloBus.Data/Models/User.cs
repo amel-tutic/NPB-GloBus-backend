5496bacd0924f2704b02f75cc83c82a047ff5742
Mirza Salkovic <salkovic.mirza@gmail.com>
2024-01-13 13:43:08 +0100

    add user model new atributs

--- GloBus.Data/Models/User.cs
+++ GloBus.Data/Models/User.cs
@@ -18,6 +18,8 @@ namespace GloBus.Data.Models
         public string City { get; set; }
         public string PhoneNumber { get; set; }
         public string Gender { get; set; }
-
+        public string Role { get; set; } = "passenger";
+        public bool isApproved { get; set; } = false;
+        public float Credit { get; set; } = 0.00f;
     }
 }
