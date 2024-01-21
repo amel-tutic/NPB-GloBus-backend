552c7b00a4fca91412aab7a543ad92b87266fb25
amel <amelstutic@gmail.com>
2024-01-21 19:49:01 +0100

    Add Penalty and PenaltyDTO

---
+++ GloBus.Data/Models/Penalty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloBus.Data.Models
+{
+    public class Penalty
+    {
+        public int Id { get; set; }
+        public int PassengerId { get; set; }
+        public int InspectorId { get; set; }
+        public DateTime DateOfPenalty { get; set; }
+        public float Price { get; set; }
+    }
+}
