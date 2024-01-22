3b3e17bdbf94246fc50395c85958e58ca508814a
amel <amelstutic@gmail.com>
2024-01-22 23:42:43 +0100

    Add unadded repositories' interfaces to UnitOfWork constructor and implement them

--- GloBus.Infrastructure/UnitOfWork.cs
+++ GloBus.Infrastructure/UnitOfWork.cs
@@ -9,25 +9,33 @@ namespace GloBus.Infrastructure
 {
     public class UnitOfWork : IUnitOfWork
     {
-        public UnitOfWork(IUsersRepository UsersRepository)
+        public UnitOfWork(IUsersRepository UsersRepository,
+                          ITicketsRepository TicketsRepository,
+                          ITicketTypesRepository TicketTypesRepository,
+                          ILinesRepository LinesRepository,
+                          IActiveTicketsRepository ActiveTicketsRepository,
+                          IInvalidTicketsRepository InvalidTicketsRepository,
+                          IPenaltiesRepository PenaltiesRepository,
+                          IRegionsRepository RegionsRepository
+                         )
         {
             this.UsersRepository = UsersRepository;
+            this.TicketsRepository = TicketsRepository;
+            this.TicketTypesRepository = TicketTypesRepository;
+            this.LinesRepository = LinesRepository;
+            this.ActiveTicketsRepository = ActiveTicketsRepository;
+            this.InvalidTicketsRepository = InvalidTicketsRepository;
+            this.PenaltiesRepository = PenaltiesRepository;
+            this.RegionsRepository = RegionsRepository;
         }
 
         public IUsersRepository UsersRepository { get; }
-
         public ITicketsRepository TicketsRepository { get; }
-
         public ITicketTypesRepository TicketTypesRepository { get; }
-
         public ILinesRepository LinesRepository { get; }
-
         public IActiveTicketsRepository ActiveTicketsRepository { get; }
-
         public IInvalidTicketsRepository InvalidTicketsRepository { get; }
-
         public IPenaltiesRepository PenaltiesRepository { get; }
-
         public IRegionsRepository RegionsRepository { get; }
     }
 }
