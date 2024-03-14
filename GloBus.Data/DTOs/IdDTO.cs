74c0121073d5b06bbe6cec2450598ebf35653774
amel <amelstutic@gmail.com>
2024-03-14 14:46:18 +0100

    Add IdDTO

---
+++ GloBus.Data/DTOs/IdDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloBus.Data.DTOs
+{
+    public class IdDTO
+    {
+        public int Id { get; set; }
+    }
+}
