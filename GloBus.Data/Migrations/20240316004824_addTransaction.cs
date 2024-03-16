fc22dbbacebb91fa13e6fae5d4d650a5e2037b9b
Mirza Salkovic <salkovic.mirza@gmail.com>
2024-03-16 12:27:40 +0100

    add new migrations for TransactionRequest

---
+++ GloBus.Data/Migrations/20240316004824_addTransaction.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GloBus.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addTransaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TransactionRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Credit = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionRequests", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TransactionRequests");
+        }
+    }
+}
